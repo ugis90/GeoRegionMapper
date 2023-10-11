cf6a9d7f04fc519062c2791cece39810637430a8
Jurgis Andziulis <jurand1@ktu.lt>
2023-10-11 14:00:16 +0300

    Add project files.

---
+++ internship/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace internship
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			string regionsJson = File.ReadAllText("regions.json");
+			string locationsJson = File.ReadAllText("locations.json");
+
+			List<Region>? regions = JsonSerializer.Deserialize<List<Region>>(regionsJson);
+			List<Location>? locations = JsonSerializer.Deserialize<List<Location>>(locationsJson);
+
+			if (regions == null || locations == null)
+			{
+				Console.WriteLine("Failed to deserialize regions or locations");
+				return;
+			}
+
+			List<MatchedRegion> results = (from region in regions
+										   let matchedLocations = (from polygon in region.Polygons
+																   from location in locations
+																   where TaskUtils.IsPointInPolygon(location.Coordinates, polygon) // check if the location is inside the polygon
+																   select location.Name).ToList() // select the name of the matched locations
+										   select new MatchedRegion { Region = region.Name, MatchedLocations = matchedLocations }).ToList();
+
+			string resultsJson = JsonSerializer.Serialize(results);
+			File.WriteAllText("results.json", resultsJson);
+		}
+	}
+}
